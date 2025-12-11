2fb9b0511763957a83c3289b6068716506ec45ad
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-12-11 16:56:53 +0100

    Do css animation exercises

--- advanced-html-css/animation/03-dropdown-menu/script.js
+++ advanced-html-css/animation/03-dropdown-menu/script.js
@@ -2,14 +2,12 @@ const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
-menuTitle.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) {
-    dropdownMenu.classList.toggle("visible");
-  }  
-})
+menuTitle.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("visible");
+});
 
 window.addEventListener("click", (e) => {
   if (!dropdownContainer.contains(e.target)) {
-    dropdownMenu.classList.remove("visible")
+    dropdownMenu.classList.remove("visible");
   }
-})
+});
